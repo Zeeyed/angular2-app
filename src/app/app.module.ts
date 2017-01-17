9e667499b47a09ae39600721fed7d8809c5a25fe
Zied <zied.ghalleb@gmail.com>
2017-01-17 12:25:45 +0100

    firebase authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+import { AuthService } from './auth.service';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCGpkfxZJhOgsitZ6itKSEGS94zemJJtQk',
+  authDomain: 'angular2-app-fd7c5.firebaseapp.com',
+  databaseURL: 'https://angular2-app-fd7c5.firebaseio.com',
+  storageBucket: 'angular2-app-fd7c5.appspot.com',
+  messagingSenderId: '341902767954'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    routes
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
